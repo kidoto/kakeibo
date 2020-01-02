6c3fa036699ecacce6f3f3929711cbbf5d9e82f7
kidoto <kidotonenene@gmail.com>
2020-01-02 22:36:58 +0900

    create prototype page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <h3>家計簿</h3>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
